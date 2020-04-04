6c9ec2516a090c3ffcf522a8e0c377d941e5de61
illitus <illitus@users.noreply.github.com>
2020-04-04 18:40:56 +0530

    added delay mitigation
    updated the code to deal with time variables
    added distance csv

---
+++ distance_up.py
@@ -0,0 +1,41 @@
+# importing csv module
+import csv
+import datetime
+
+# csv file name
+filename = "distance.csv"
+
+# initializing the titles and rows list
+fields = []
+rows = []
+
+# reading csv file
+with open(filename, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+
+    # extracting field names through first row
+    fields = next(csvreader)
+
+    # extracting each data row one by one
+    for row in csvreader:
+        rows.append(row)
+
+# print(rows)
+
+a = 'Charni Road'
+b = 'Mahalaxmi'
+dist_a = 0
+dist_b = 0
+for i in range(0, len(rows)):
+    if rows[i][0] == a:
+        dist_a = float(rows[i][1])
+
+    if rows[i][0] == b:
+        dist_b = float(rows[i][1])
+
+print(dist_b-dist_a)
+
+z = "13:12"
+z = z.split(":")
+print(int(z[0]))
--- mitigation.py
+++ mitigation.py
@@ -1,9 +1,9 @@
 # importing csv module
 import csv
-import datetime
+from datetime import datetime, timedelta
 
 # csv file name
-filename = "distance.csv"
+filename = "churchgate_up.csv"
 
 # initializing the titles and rows list
 fields = []
@@ -21,4 +21,114 @@ with open(filename, 'r') as csvfile:
     for row in csvreader:
         rows.append(row)
 
-#testing
+
+# fetching current time
+x = datetime.now()
+x = x.strftime("%H") + ":" + x.strftime("%M")
+
+# converting x into a time variable again
+x = x.split(":")
+x = timedelta(hours=int(x[0]), minutes=int(x[1]))
+
+# checking running trains
+transit = []
+
+for i in range(0, len(rows)):
+    schedule = []
+    details = []
+    stations = []
+    for j in range(3, len(rows[i])):
+        if rows[i][j]:
+            # schedule.append(rows[i][j])
+            t = rows[i][j]
+            t = t.split(":")
+            t = timedelta(hours=int(t[0]), minutes=int(t[1]))
+            schedule.append(t)
+            stations.append(fields[j])
+
+    # print(schedule)
+
+    if schedule[0] < x < schedule[-1]:
+        # print("running: ", rows[i][0])
+        transit.append(int(rows[i][0]))
+
+
+# current status data
+print(transit)
+
+route = str(input("Enter Route Code: "))
+
+schedule = []
+stations = []
+
+for i in range(0, len(rows)):
+    if route == rows[i][0]:
+        for j in range(3, len(rows[i])):
+            if rows[i][j]:
+                t = rows[i][j]
+                t = t.split(":")
+                t = timedelta(hours=int(t[0]), minutes=int(t[1]))
+                schedule.append(t)
+                stations.append(fields[j])
+
+# distance data
+
+
+# csv file name
+filename_d = "distance_up.csv"
+
+# initializing the titles and rows list
+fields_d = []
+rows_d = []
+
+# reading csv file
+with open(filename_d, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+
+    # extracting field names through first row
+    fields_d = next(csvreader)
+
+    # extracting each data row one by one
+    for row in csvreader:
+        rows_d.append(row)
+
+
+flag = True
+
+# for trains in stations
+for k in range(0, len(schedule)):
+    if schedule[k] == x:
+        # s = True
+        a = stations[k]
+        b = stations[k+1]
+        a_t = schedule[k]
+        b_t = schedule[k+1]
+        i_dist = 0
+        flag = False
+        break
+
+    # for trains not in stations
+if flag:
+    for k in range(0, len(schedule)):
+        if schedule[k] > x:
+            # s = False
+            a = stations[k - 1]
+            b = stations[k]
+            a_t = schedule[k-1]
+            b_t = schedule[k]
+            time = b_t - a_t
+            print(time)
+            flag = False
+            break
+
+# loc = str(input("Enter Departing Station: "))
+dist_rl = float(input("Enter Distance Covered: "))
+time = str(time)
+time = time.split(":")
+print(dist_rl/float(time[1]))
+
+
+
+
+
